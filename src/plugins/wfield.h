14435e9513fc9446038ee07a2f2e52bf4abdb08d
Dmintriy L. Kruglikov <master@shadow.breg.pl.ua>
2008-06-24 11:55:37 +0300

    Repaired src/plugins wfield.h

--- src/plugins/wfield.h
+++ src/plugins/wfield.h
@@ -143,41 +143,4 @@ class  wCheckBox: public QCheckBox
 		void valueChanged(const QString &);
 };
 
-/*!
- * \en PopupCalendar for DateEdit field editor. \_en
- * \ru
- * 	\brief Всплывающий календарь для выбора даты.
- *
- * 	Используется как дополнение к виджету поля ввода/редактирования
- * 	атрибута типа Дата бизнес объекта Ананаса.
- *
- * \_ru
- */
-class PopupCalendar : public QDialog
-{
-Q_OBJECT
-public:
-	PopupCalendar(const QDate &day = QDate::currentDate(),
-		QPoint pos = QPoint(), QWidget *parent = 0, const char *name = 0);
-
-	static QDate	getDate(QWidget *parent = 0, const QDate &day = QDate::currentDate(),
-	QPoint		pos = QPoint());
-
-	const QDate&	day() const { return today; }
-	void		setDay(const QDate &day);
-	QSize		sizeHint() const;
-
-protected:
-	void mousePressEvent(QMouseEvent *event);
-	void mouseDoubleClickEvent(QMouseEvent *) { accept(); }
-	void keyPressEvent(QKeyEvent *event);
-	void paintEvent(QPaintEvent *event);
-
-private:
-	enum { ROWS = 7, COLS = 7 };
-	QFont smallFont;
-	QDate today;
-	const QDate original;
-};
-
 #endif
